7525ca9e4e9f6714c34ec0ae27290587583640fb
donghwy98 <donghwy98@naver.com>
2021-11-02 16:12:59 +0900

    인프런_스프링 입문

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,43 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+/**
+    //private final DataSource dataSource;
+    private EntityManager em;
+
+    //public SpringConfig(DataSource dataSource) {
+    //    this.dataSource = dataSource;
+    //}
+    @Autowired
+    public SpringConfig(EntityManager em){
+        this.em = em;
+    }
+*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+    /**@Bean
+    public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }*/
+}
